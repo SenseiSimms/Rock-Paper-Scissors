6997fe3d7f24c32de220616803d9c632bdbea54a
SenseiSimms <luciddreamstates@gmail.com>
2022-10-09 21:39:29 -0500

    10-09-22 adding score counter to javascript.

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+
+
+
 function getComputerChoice () {
     const moves = ['ROCK', 'PAPER', 'SCISSORS'];
     return moves[Math.floor(Math.random() * moves.length)];
@@ -5,17 +8,54 @@ function getComputerChoice () {
 
 function playRound(playerChoice, compChoice) {
     if (playerChoice === compChoice) {
-        return 'Tie Game';
-    } else if (playerChoice == 'ROCK') {
-        if (compChoice == 'PAPER') {return lose;} else{return win;}
-    }
+        return tieRound;
+    } else if (playerChoice == 'ROCK' && compChoice == 'SCISSORS') {
+        return winRound; } 
+        else if (playerChoice == 'PAPER' && compChoice == 'ROCK'){return winRound;} 
+        else if (playerChoice == 'SCISSORS' && compChoice == 'PAPER') {return winRound;} else {return loseRound;}
 }
 
+let tieRound = 'Tie Game';
 let playerChoice = 'rock';
 const playerResult = playerChoice.toUpperCase();
 const compChoice = getComputerChoice();
-let win = 'You win, ' + playerResult + ' beats ' + compChoice + '.';
-let lose = 'You lose, ' + playerResult + ' loses to ' + compChoice + '.';
+let winRound = 'You win, ' + playerResult + ' beats ' + compChoice + '.';
+let loseRound = 'You lose, ' + playerResult + ' loses to ' + compChoice + '.';
+let playerWinsGame = 'Congratulations, you win the match!';
+let compWinsGame = 'Sorry you lost, computer wins the match!';
 
+function game() {
+for (let i = 0; i < 5; i++) {
+    let playerChoice = prompt('Rock, Paper, or Scissors?');
+    let roundResult = playRound(playerChoice, compChoice);
+    console.log(roundResult);
+    console.log('Your score is  ' + playerScore);
+    console.log('The computer score is  ' + compScore);
+    if (playerScore === 5 || compScore === 5) {
+        break;
+    }
+}
+}
 
-console.log(playRound(playerResult, compChoice))
+let playerScore = 0;
+let compScore = 0;
+let tieScore = 0;
+
+function gameScore (result) {
+    let result = playRound();
+
+    if (result === winRound) {
+        playerScore++;
+    } else if (result === tieRound) {
+        tieScore++;
+    } else {compScore++;}
+
+    if (playerScore === 5) {console.log(playerWinsGame);
+}
+
+if (compScore === 5) {
+    console.log(compWinsGame);
+}
+
+}
+
