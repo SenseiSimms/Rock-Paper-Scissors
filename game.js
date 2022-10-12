ea1a4bc08bfd4bc11ba65f9b9937808d90704555
SenseiSimms <luciddreamstates@gmail.com>
2022-10-11 23:40:18 -0500

    10-11-2022 adding score counter to JS section.

--- game.js
+++ game.js
@@ -1,61 +1,58 @@
 
 
+// Line of code to let the computer choose randomly.
 
 function getComputerChoice () {
     const moves = ['ROCK', 'PAPER', 'SCISSORS'];
     return moves[Math.floor(Math.random() * moves.length)];
 }
 
+let playerScore = 0;
+    let compScore = 0;
+
+// Plays a single round of paper,rock,scissors
+
 function playRound(playerChoice, compChoice) {
+let tieRound = 'Tie Game';
+let winRound = 'You win, ' + playerChoice + ' beats ' + compChoice + '.';
+let loseRound = 'You lose, ' + playerChoice + ' beats ' + compChoice + '.';
+
+
+
     if (playerChoice === compChoice) {
         return tieRound;
     } else if (playerChoice == 'ROCK' && compChoice == 'SCISSORS') {
-        return winRound; } 
-        else if (playerChoice == 'PAPER' && compChoice == 'ROCK'){return winRound;} 
-        else if (playerChoice == 'SCISSORS' && compChoice == 'PAPER') {return winRound;} else {return loseRound;}
-}
+        playerScore++;
+        return winRound;} 
+        else if (playerChoice == 'PAPER' && compChoice == 'ROCK'){
+            playerScore++;
+            return winRound;} 
+        else if (playerChoice == 'SCISSORS' && compChoice == 'PAPER') {return winRound;} else {
+            compScore++;
+            return loseRound;} }
 
-let tieRound = 'Tie Game';
-let playerChoice = 'rock';
-const playerResult = playerChoice.toUpperCase();
-const compChoice = getComputerChoice();
-let winRound = 'You win, ' + playerResult + ' beats ' + compChoice + '.';
-let loseRound = 'You lose, ' + playerResult + ' loses to ' + compChoice + '.';
-let playerWinsGame = 'Congratulations, you win the match!';
-let compWinsGame = 'Sorry you lost, computer wins the match!';
 
 function game() {
-for (let i = 0; i < 5; i++) {
-    let playerChoice = prompt('Rock, Paper, or Scissors?');
-    let roundResult = playRound(playerChoice, compChoice);
+for (let i = 0; i < 8; i++) {
+    
+    let playerWinsGame = 'Congratulations, you win the match!';
+let compWinsGame = 'Sorry you lost, computer wins the match!';
+    let playerChoice = prompt('Choose your move, Rock Paper or Scissors?');
+const playerResult = playerChoice.toUpperCase();
+const compChoice = getComputerChoice();
+    let roundResult = playRound(playerResult, compChoice);
     console.log(roundResult);
     console.log('Your score is  ' + playerScore);
     console.log('The computer score is  ' + compScore);
-    if (playerScore === 5 || compScore === 5) {
-        break;
+    if (playerScore > 5 || compScore > 5) {
+        break;}
+    if (playerScore > compScore) 
+    {
+        return playerWinsGame;} else if (compScore > playerScore) {
+            return compWinsGame;
+        }
     }
 }
-}
 
-let playerScore = 0;
-let compScore = 0;
-let tieScore = 0;
 
-function gameScore (result) {
-    let result = playRound();
-
-    if (result === winRound) {
-        playerScore++;
-    } else if (result === tieRound) {
-        tieScore++;
-    } else {compScore++;}
-
-    if (playerScore === 5) {console.log(playerWinsGame);
-}
-
-if (compScore === 5) {
-    console.log(compWinsGame);
-}
-
-}
-
+game()
