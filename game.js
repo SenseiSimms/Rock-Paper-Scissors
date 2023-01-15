29ede11fa13a2c3bf832010e39f3363df71da756
SenseiSimms <luciddreamstates@gmail.com>
2023-01-15 17:31:31 -0600

    1/15/2023 refactor code for paper rock scissors ui

--- game.js
+++ game.js
@@ -5,60 +5,111 @@ function getComputerChoice () {
     return moves[Math.floor(Math.random() * moves.length)];
 }
 
+function resetGame() {
+    //Resets game
+
+
+}
+
 // Sets the player and computer score to 0
 
 let playerScore = 0;
+
 let compScore = 0;
 
-// Plays a single round of paper,rock,scissors
+let tieScore = 0;
 
-function playRound(playerChoice, compChoice) {
-let tieRound = 'Tie Game, both of you picked ' + playerChoice + '.';
-let winRound = 'You win, ' + playerChoice + ' beats ' + compChoice + '.';
-let loseRound = 'You lose, ' + playerChoice + ' beats ' + compChoice + '.';
+function updatePlayerScore() {
+    console.log(`${playerScore}`);
+
+    const playerScoreResult = document.getElementById("p1Score")
+
+    playerScoreResult.textContent = playerScore;
+};
+
+function updateCompScore() {
+    console.log(`${compScore}`);
+
+    const compScoreResult = document.getElementById("compScore")
+
+    compScoreResult.textContent = compScore;
+};
+
+function updateTieScore() {
+    console.log(`${tieScore}`);
+
+    const tieScoreResult = document.getElementById("tieScore")
+
+    tieScoreResult.textContent = tieScore;
+};
 
 
-// sets up a round of rock,paper, scissors and the conditions for each turn
 
-    if (playerChoice === compChoice) {
-        return tieRound
-    } else if (playerChoice == 'ROCK' && compChoice == 'SCISSORS') {
-        playerScore++
-        return winRound} 
-        else if (playerChoice == 'PAPER' && compChoice == 'ROCK'){
-            playerScore++
-            return winRound} 
-        else if (playerChoice == 'SCISSORS' && compChoice == 'PAPER') {return winRound} else {
-            compScore++
-            return loseRound} 
-        }
 
-// plays multiple rounds and determines a winner based off who has a higher score at the end
+const compChoice = getComputerChoice();
+const playerChoice = document.querySelector("btn");
 
-function game() {
-for (let i = 0; i < 5; i++) {
-    let playerChoice = prompt('Choose your move; Rock, Paper, or Scissors?');
-    const playerResult = playerChoice.toUpperCase();
-    const compChoice = getComputerChoice();
-    let roundResult = playRound(playerResult, compChoice);
-    console.log(roundResult);
-    console.log('Your score is  ' + playerScore);
-    console.log('The computer score is  ' + compScore);
-    }
+let tieRound = `Tie round, both of you picked ${playerChoice}.`;
+let winRound = `You win, ${playerChoice} beats ${compChoice}.`;
+let loseRound = `You lose, ${compChoice} beats ${playerChoice}.`;
 
-    let playerWinsGame = 'Congratulations, you win the match!';
+let playerWinsGame = 'Congratulations, you win the match!';
 let compWinsGame = 'Sorry you lost, computer wins the match!';
 
-if (playerScore > compScore) 
+
+function playRound(playerChoice, compChoice) {
+    if (playerChoice === compChoice) {tieScore++
+        updateTieScore()
+        return tieRound
+    } else if (playerChoice == 'ROCK' && compChoice == 'SCISSORS' || playerChoice == 'PAPER' && compChoice == 'ROCK' || playerChoice == 'SCISSORS' && compChoice == 'PAPER') {
+        playerScore++
+        updatePlayerScore()
+        return winRound} 
+        else {
+            compScore++
+            updateCompScore()
+            return loseRound};
+        };
+
+
+
+
+
+function startGame() {
+    
+    //plays the game until someone wins 5 rounds
+
+    let buttons = document.querySelectorAll("button")
+    buttons.forEach((button) => buttons.addEventListener(('click'), () => {
+        if (button.id) {
+            playRound(button.id);
+        }}
+    )
+    );
+
+
+    if (playerScore > compScore && playerScore >= 5) 
     {return playerWinsGame} else 
-    if (compScore > playerScore) {
+    if (compScore > playerScore && compScore >= 5) {
             return compWinsGame
         } else
         {
             return 'Tie Match, good job!';
-    }
-}
+}};
+
+
+// Plays a single round of paper,rock,scissors
+
+
+
+// sets up a round of rock,paper, scissors and the conditions for each turn
+
+    
+    
+// plays multiple rounds and determines a winner based off who has a higher score at the end
+
+
     
 
 
-console.log(game())
+
